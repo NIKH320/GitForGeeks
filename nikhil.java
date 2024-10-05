bd20251bf5047c297f31a23ed7a76d52a659e985
NIKH320 <damahenm@rknec.edu>
2024-10-05 15:35:52 +0530

    my first2 changes

---
+++ nikhil.java
@@ -0,0 +1,14 @@
+package GitForGeeks;
+
+/**
+ * nikhil
+ */
+public class nikhil {
+
+    
+}class Nikhil 
+{
+    public static void main(String[] args) {
+        System.out.println("NIKHIL");
+    }
+}
